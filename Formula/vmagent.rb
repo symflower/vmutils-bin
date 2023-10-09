216c6b55f974b8b3b2768300a43f803651af05c9
Markus Zimmermann <markus.zimmermann@symflower.com>
2023-10-09 19:00:38 +0200

    refactor, Format consistently

--- Formula/vmagent.rb
+++ Formula/vmagent.rb
@@ -5,59 +5,59 @@ class Vmagent < Formula
 	version "1.94.0"
 
 	on_macos do
-	  on_intel do
-	    checksumAmd64 = "05bb884f03fa1164fdee87b1bafdc84da57d0063a6b280c3fbac36fff8c0b2ed" // The wording of this variable is intentional for easier automation.
+		on_intel do
+			checksumAmd64 = "05bb884f03fa1164fdee87b1bafdc84da57d0063a6b280c3fbac36fff8c0b2ed" // The wording of this variable is intentional for easier automation.
 
-	    url "https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v#{version}/vmutils-darwin-amd64-v#{version}.tar.gz"
-	    sha256 "#{checksumAmd64}"
-	  end
+			url "https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v#{version}/vmutils-darwin-amd64-v#{version}.tar.gz"
+			sha256 "#{checksumAmd64}"
+		end
 
-	  on_arm do
-		checksumArm64 = "121052826e4c80108ba17670393efbd4a32326f8c78b4664898810eaadf6e99d" // The wording of this variable is intentional for easier automation.
-	
-		url "https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v#{version}/vmutils-darwin-arm64-v#{version}.tar.gz"
-		sha256 "#{checksumArm64}"
-	  end
+		on_arm do
+			checksumArm64 = "121052826e4c80108ba17670393efbd4a32326f8c78b4664898810eaadf6e99d" // The wording of this variable is intentional for easier automation.
+
+			url "https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v#{version}/vmutils-darwin-arm64-v#{version}.tar.gz"
+			sha256 "#{checksumArm64}"
+		end
 	end
 
 	def caveats
-	  <<~EOS
-		When run from `brew services`, `vmagent` is run from
-		`vmagent_brew_services` and uses the flags in:
-		  #{etc}/vmagent/service.args
+		<<~EOS
+			When run from `brew services`, `vmagent` is run from
+			`vmagent_brew_services` and uses the flags in:
+				#{etc}/vmagent/service.args
 
-		A default `scrape-config` can be found at #{etc}/vmagent/scrape.yml
-	  EOS
+			A default `scrape-config` can be found at #{etc}/vmagent/scrape.yml
+		EOS
 	end
 
 	def install
-	  bin.install "vmagent-prod" => "vmagent"
+		bin.install "vmagent-prod" => "vmagent"
 
-	  (bin/"vmagent_brew_services").write <<~EOS
-		#!/bin/bash
-		exec #{bin}/vmagent $(<#{etc}/vmagent/service.args)
-	  EOS
-
-	  unless File.exists?(etc/"vmagent/scrape.yml")
-		(etc/"vmagent/scrape.yml").write <<~EOS
-			global:
-			scrape_interval: 10s
-			scrape_configs:
-			- job_name: "vmagent"
-				static_configs:
-				- targets: ["127.0.0.1:8429"]
+		(bin/"vmagent_brew_services").write <<~EOS
+			#!/bin/bash
+			exec #{bin}/vmagent $(<#{etc}/vmagent/service.args)
 		EOS
-	  end
+
+		unless File.exists?(etc/"vmagent/scrape.yml")
+			(etc/"vmagent/scrape.yml").write <<~EOS
+				global:
+				scrape_interval: 10s
+				scrape_configs:
+				- job_name: "vmagent"
+				  static_configs:
+				  - targets: ["127.0.0.1:8429"]
+			EOS
+		end
 	end
 
 	service do
-	  run [opt_bin/"vmagent_brew_services"]
-	  keep_alive false
-	  log_path var/"log/vmagent.log"
-	  error_log_path var/"log/vmagent.err.log"
+		run [opt_bin/"vmagent_brew_services"]
+		keep_alive false
+		log_path var/"log/vmagent.log"
+		error_log_path var/"log/vmagent.err.log"
 	end
 
 	test do
-	  system "#{bin}/vmagent", "--help"
+		system "#{bin}/vmagent", "--help"
 	end
  end
