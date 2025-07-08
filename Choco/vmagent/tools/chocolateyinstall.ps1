3adf7e59ad8c6f993aba178ff181efdb6b88e472
zimmski <zimmski@users.noreply.github.com>
2025-07-08 10:24:58 +0000

    Update VM Version to null

--- Choco/vmagent/tools/chocolateyinstall.ps1
+++ Choco/vmagent/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageName = $env:ChocolateyPackageName
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $swversion = "3.0.0-alpha.11"
-$vmutilsChecksum = "29cba1df61467031ef0ace2bd55c169064f736739d53aa7ef6669bcc5c1c286d"
+$vmutilsChecksum = ""
 $winswChecksum = "a2daa6a33a9c2b791ae31d9092e7935c339d1e03e89bfb747618ce2f4e819e20"
 
 $vmutilsURL = "https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v${env:ChocolateyPackageVersion}/vmutils-windows-amd64-v${env:ChocolateyPackageVersion}.zip"
--- Formula/vmagent.rb
+++ Formula/vmagent.rb
@@ -2,18 +2,18 @@ class Vmagent < Formula
 	desc "VMAgent is a tiny agent which helps you collect metrics from various sources, relabel and filter the collected metrics and store them in VictoriaMetrics or any other storage systems via Prometheus remote_write protocol."
 	homepage "https://docs.victoriametrics.com/vmagent.html"
 	license all_of: ["MIT", "Apache-2.0"]
-	version "1.120.0"
+	version "null"
 
 	on_macos do
 		on_intel do
-			checksumAmd64 = "64d05bbaddabab237436768d54c97b5fd2cc91ce33d48c3451dc728f484e5ffc" # The wording of this variable is intentional for easier automation.
+			checksumAmd64 = "" # The wording of this variable is intentional for easier automation.
 
 			url "https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v#{version}/vmutils-darwin-amd64-v#{version}.tar.gz"
 			sha256 "#{checksumAmd64}"
 		end
 
 		on_arm do
-			checksumArm64 = "55a87a1b8b91c954eb3fbd543cfadb392b5bdf1f7f41ef568576178c6385b4e4" # The wording of this variable is intentional for easier automation.
+			checksumArm64 = "" # The wording of this variable is intentional for easier automation.
 
 			url "https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v#{version}/vmutils-darwin-arm64-v#{version}.tar.gz"
 			sha256 "#{checksumArm64}"
