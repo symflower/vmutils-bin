b425f46a86729c256cfd287faa375710c6c389e6
zimmski <zimmski@users.noreply.github.com>
2025-12-15 10:30:41 +0000

    Update VM Version to 1.132.0

--- Choco/vmagent/tools/chocolateyinstall.ps1
+++ Choco/vmagent/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageName = $env:ChocolateyPackageName
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $swversion = "3.0.0-alpha.11"
-$vmutilsChecksum = "6a5be5ebdc7455ad56e73b490cd94601bb203ddafe363c5482b8313192b189dd"
+$vmutilsChecksum = "4a8a201d93d0ede589107b2771a2517a518138feaa49c7156ea21bb610f1f97c"
 $winswChecksum = "a2daa6a33a9c2b791ae31d9092e7935c339d1e03e89bfb747618ce2f4e819e20"
 
 $vmutilsURL = "https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v${env:ChocolateyPackageVersion}/vmutils-windows-amd64-v${env:ChocolateyPackageVersion}.zip"
